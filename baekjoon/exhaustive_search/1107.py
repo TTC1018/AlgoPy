fcd47d6950d3475923c1b8b9f7dc78fe82443769
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-07-16 01:38:45 +0900

    1107 ongoing

--- baekjoon/exhaustive_search/1107.py
+++ baekjoon/exhaustive_search/1107.py
@@ -4,12 +4,15 @@ n = int(input())
 m = int(input())
 nums = [int(('').join(n)) for n in input().split(' ')]
 
+if(n==100):
+    print(0)
+    exit()
+
 start = 100
 
 num_split = [int(('').join(num)) for num in list(str(n))]
-num_split = set(num_split)
+num_split = list(set(num_split))
 
-if nums in num_split:
+if set(nums).issubset(set(num_split)):
     print("YES")
 
-print(num_split)
