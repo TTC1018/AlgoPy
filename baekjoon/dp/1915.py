22bdbafa2060bc137c51b5692cf7dbe2bc38a209
DongHyeon Park <paramore@kakao.com>
2021-07-22 18:26:06 +0900

    1915 ongoing

---
+++ baekjoon/dp/1915.py
@@ -0,0 +1,17 @@
+n, m = [int(''.join(n)) for n in input().split(' ')]
+nums = []
+dp = []
+for i in range(n):
+    nums.append([int(n) for n in input()])
+    dp.append([0] * m)
+
+
+if nums[0][0] == 0:
+    dp[0][0] == 0
+else:
+    dp[0][0] == 1
+
+dp[i] == 0:
+
+
+dp[i] == 1:
--- baekjoon/exhaustive_search/1697.py
+++ baekjoon/exhaustive_search/1697.py
@@ -1,47 +1,22 @@
+from collections import deque
+
 N, K = [int(''.join(n)) for n in input().split(' ')]
+visited = [0] * 100001
 
-def minus_one(N, sum):
-    global answer
 
-    if(N == K):
-        return
+def solve():
+    q = deque()
+    q.append(N)
 
-    new_N = N-1
+    while q:
+        X = q.popleft()
+        if X == K:
+            print(visited[X])
+            break
+        for next in [X-1, X+1, X*2]:
+            if 0 <= next <= 100000 and not visited[next]:
+                visited[next] = visited[X] + 1
+                q.append(next)
 
-    answer += 1
-    minus_one(new_N)
-    plus_one(new_N)
-    double_one(new_N)
 
-def plus_one(N):
-    global answer
-
-    if (N == K):
-        return
-
-    new_N = N + 1
-
-    answer += 1
-    minus_one(new_N)
-    plus_one(new_N)
-    double_one(new_N)
-
-def double_one(N):
-    global answer
-
-    if (N == K):
-        return
-
-    new_N = N * 2
-
-    answer += 1
-    minus_one(new_N)
-    plus_one(new_N)
-    double_one(new_N)
-
-answer = 0
-minus_one(N)
-plus_one(N)
-double_one(N)
-
-print(answer)
+solve()
