d3916351e6e798c789f07a5e5de48d1c29c43dad
DongHyeon Park <paramore@kakao.com>
2021-07-22 21:08:16 +0900

    Update 1915.py

--- baekjoon/dp/1915.py
+++ baekjoon/dp/1915.py
@@ -5,13 +5,27 @@ for i in range(n):
     nums.append([int(n) for n in input()])
     dp.append([0] * m)
 
+answer = 0
+# 초기화
+for i in range(n):
+    if nums[i][0] == 1:
+        dp[i][0] = 1
+        answer = 1
+    else:
+        dp[i][0] = 0
+for i in range(m):
+    if nums[0][i] == 1:
+        dp[0][i] = 1
+        answer = 1
+    else:
+        dp[0][i] = 0
 
-if nums[0][0] == 0:
-    dp[0][0] == 0
-else:
-    dp[0][0] == 1
+# DP
+if n > 1 and m > 1:
+    for i in range(1, n):
+        for j in range(1, m):
+            if nums[i][j] != 0:
+                dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1], i, j) + 1
+                answer = max(answer, dp[i][j])
 
-dp[i] == 0:
-
-
-dp[i] == 1:
+print(answer ** 2)
