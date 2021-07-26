dbca672fb56d363def22390a31ec5363a4bd5061
DongHyeon Park <paramore@kakao.com>
2021-07-26 18:21:47 +0900

    Update 1963.py

--- baekjoon/exhaustive_search/1963.py
+++ baekjoon/exhaustive_search/1963.py
@@ -12,3 +12,37 @@ T = int(input())
 for i in range(T):
     A, B = [int(''.join(n)) for n in input().split(' ')]
     answer = 0
+
+    i = 0
+    while True:
+        if A == B:
+            if prime(A):
+                print(answer)
+            else:
+                print("Impossible")
+            break
+
+        if prime(A):
+            i += 1
+
+        strA = int(str(A)[i])
+        if i == 0:
+            nums = [n for n in range(9, 0, -1) if n != strA]
+        else:
+            nums = [n for n in range(9, -1, -1) if n != strA]
+
+        for n in nums:
+            A = str(A)
+            A[i] = str(n)
+            A = int(A)
+
+            if prime(A):
+                answer += 1
+                i += 1
+                break
+            else:
+                continue
+
+
+
+
