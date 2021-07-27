214b3f42e8c49d5d36c8fdbde73d5a9d75f83542
DongHyeon Park <paramore@kakao.com>
2021-07-27 18:53:31 +0900

    Update 1963.py

--- baekjoon/exhaustive_search/1963.py
+++ baekjoon/exhaustive_search/1963.py
@@ -1,18 +1,39 @@
+from collections import deque
+import math
+import copy
+
 def prime(num):
     if num < 2:
         return False
 
-    for i in range(2, num // 2 + 1):
+    for i in range(2, int(math.sqrt(num)) + 1):
         if num % i == 0:
             return False
     return True
 
 
-def solve(num, nums, visited):
-    visited[]
+def solve(A, B):
+    global answer
+    q = deque([[list(str(A)), 0]])
+    visited = {A}
 
-    for n in nums:
-
+    while True:
+        num, answer = q.popleft()
+        if int(''.join(num)) == B:
+            print(answer)
+            return
+        else:
+            for i in range(4):
+                for j in range(10):
+                    if num[i] == j:
+                        continue
+                    else:
+                        temp = copy.deepcopy(num)
+                        temp[i] = str(j)
+                        temp = int(''.join(temp))
+                        if temp not in visited and temp > 1000 and prime(temp):
+                            visited.add(temp)
+                            q.append([temp, answer + 1])
 
 
 T = int(input())
@@ -20,35 +41,10 @@ for i in range(T):
     A, B = [int(''.join(n)) for n in input().split(' ')]
     answer = 0
 
-    i = 0
-    while True:
-        if A == B:
-            if prime(A):
-                print(answer)
-            else:
-                print("Impossible")
-            break
+    solve(A, B)
+    if A != B:
+        print("Impossible")
 
-        if prime(A):
-            i += 1
-
-        strA = int(str(A)[i])
-        if i == 0:
-            nums = [n for n in range(9, 0, -1) if n != strA]
-        else:
-            nums = [n for n in range(9, -1, -1) if n != strA]
-
-        for n in nums:
-            A = str(A)
-            A[i] = str(n)
-            A = int(A)
-
-            if prime(A):
-                answer += 1
-                i += 1
-                break
-            else:
-                continue
 
 
 
