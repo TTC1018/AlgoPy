2154d14a6df4dd89e240108bc2c5a9afe4fe8150
DongHyeon Park <paramore@kakao.com>
2021-07-31 00:04:08 +0900

    Update 2186.py

--- baekjoon/exhaustive_search/2186.py
+++ baekjoon/exhaustive_search/2186.py
@@ -3,27 +3,33 @@ from collections import deque
 N, M, K = [int(n) for n in input().split(' ')]
 alphas = []
 for i in range(N):
-    alphas.append(list(input()))
-
+    alphas.append(input())
 target = input()
+
 q = deque()
+for idx, l in enumerate(alphas):
+    if target[0] in l:
+        q.append([[idx, l.index(target[0])], 1])
 
-for i in range(N):
-    for j in range(M):
-        if alphas[i][j] == target[0]:
-            q.append(([(i, j)], target[0]))
-
-answer = []
+answer = 0
+t_len = len(target)
 while q:
-    points, word = q.popleft()
-    if word == target and points not in answer:
-        answer.append(points)
+    point, count = q.popleft()
 
-    x, y = points[-1]
-    for i in range(K):
-        
+    if count == t_len:
+        answer += 1
+        continue
+
+    x, y = point
+    for i in range(x-K, x+K + 1):
+        if 0 <= i <= N-1:
+            if alphas[i][y] == target[count]:
+                q.append([[i, y], count + 1])
+
+    for i in range(y-K, y+K + 1):
+        if 0 <= i <= M-1:
+            if alphas[x][i] == target[count]:
+                q.append([[x, i], count + 1])
 
 
-
-
-print(len(answer))
+print(answer)
