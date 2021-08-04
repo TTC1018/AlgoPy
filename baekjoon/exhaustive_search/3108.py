a15e345b2bcb90e4e0309734979d1c125dc85d2f
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-08-05 01:18:46 +0900

    Update 3108.py

--- baekjoon/exhaustive_search/3108.py
+++ baekjoon/exhaustive_search/3108.py
@@ -1,35 +1,36 @@
 N = int(input())
 
-visited = [[False] * 1001] * 1001
+visited = []
+for i in range(1001):
+    visited.append([False] * 1001)
 
 PU = 0
 
 visited[500][500] = True
+
 for i in range(N):
     PU_flag = True
     x1, y1, x2, y2 = list(map(int, input().split(' ')))
     for x_range in range(x1 + 500, x2 + 500 + 1):
-        if visited[x_range][y1] or visited[x_range][y2]:
+        if visited[x_range][y1 + 500] or visited[x_range][y2 + 500]:
             PU_flag = False
-            print('changed in ({},y)'.format(x_range))
             break
 
     if PU_flag:
         for y_range in range(y1 + 500, y2 + 500 + 1):
-            if visited[x1][y_range] or visited[x2][y_range]:
+            if visited[x1 + 500][y_range] or visited[x2 + 500][y_range]:
                 PU_flag = False
-                print('changed in (x,{})'.format(y_range))
                 break
 
     if PU_flag:
         PU += 1
 
     for x_range in range(x1 + 500, x2 + 500 + 1):
-        visited[x_range][y1] = True
-        visited[x_range][y2] = True
+        visited[x_range][y1 + 500] = True
+        visited[x_range][y2 + 500] = True
     for y_range in range(y1 + 500, y2 + 500 + 1):
-        visited[x1][y_range] = True
-        visited[x2][y_range] = True
+        visited[x1 + 500][y_range] = True
+        visited[x2 + 500][y_range] = True
 
 
 print(PU)
