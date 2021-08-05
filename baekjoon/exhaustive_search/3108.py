36ec05b2fc166f2d48d03eb3bfe552335109a97f
DongHyeon Park <paramore@kakao.com>
2021-08-05 17:19:14 +0900

    Update 3108.py

--- baekjoon/exhaustive_search/3108.py
+++ baekjoon/exhaustive_search/3108.py
@@ -1,36 +1,37 @@
+def solve(y, x):
+    points[y][x] = 2
+
+    for i, j in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        new_y = y + i
+        new_x = x + j
+        if 0 <= new_y <= 2000 and 0 <= new_x <= 2000:
+            if points[new_y][new_x] == 1:
+                solve(new_y, new_x)
+
+
 N = int(input())
 
-visited = []
-for i in range(1001):
-    visited.append([False] * 1001)
+points = []
+for i in range(2001):
+    points.append([0] * 2001)
+
+starts = []
+for i in range(N):
+    nums = list(map(int, input().split(' ')))
+    x1, y1, x2, y2 = list(map(lambda n: (n + 500) * 2, nums))
+    starts.append((x1, y1))
+
+    for x_range in range(x1, x2 + 1):
+        points[y1][x_range] = 1
+        points[y2][x_range] = 1
+    for y_range in range(y1, y2 + 1):
+        points[y_range][x1] = 1
+        points[y_range][x2] = 1
 
 PU = 0
-
-visited[500][500] = True
-
-for i in range(N):
-    PU_flag = True
-    x1, y1, x2, y2 = list(map(int, input().split(' ')))
-    for x_range in range(x1 + 500, x2 + 500 + 1):
-        if visited[x_range][y1 + 500] or visited[x_range][y2 + 500]:
-            PU_flag = False
-            break
-
-    if PU_flag:
-        for y_range in range(y1 + 500, y2 + 500 + 1):
-            if visited[x1 + 500][y_range] or visited[x2 + 500][y_range]:
-                PU_flag = False
-                break
-
-    if PU_flag:
+for sx, sy in starts:
+    if points[sy][sx] == 1:
+        solve(sy, sx)
         PU += 1
-
-    for x_range in range(x1 + 500, x2 + 500 + 1):
-        visited[x_range][y1 + 500] = True
-        visited[x_range][y2 + 500] = True
-    for y_range in range(y1 + 500, y2 + 500 + 1):
-        visited[x1 + 500][y_range] = True
-        visited[x2 + 500][y_range] = True
-
-
+PU = PU - 1 if points[1000][1000] == 2 else PU
 print(PU)
