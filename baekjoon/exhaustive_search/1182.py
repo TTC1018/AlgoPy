a808e9b86918a62bbb196e3b71a20ae7b3b18e56
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2021-08-08 22:38:56 +0900

    Update 1182.py

--- baekjoon/exhaustive_search/1182.py
+++ baekjoon/exhaustive_search/1182.py
@@ -1,22 +1,19 @@
 from collections import deque
+from itertools import combinations
 
 N, S = tuple(map(int, input().split(' ')))
 nums = list(map(int, input().split(' ')))
 
 q = deque()
 for n in nums:
-    q.append([n])
+    q.append((n, ))
 
-answer = set()
-while q:
-    x = q.popleft()
+answer = []
+for i in range(1, len(nums) + 1):
+    candidate = combinations(nums, i)
+    for c in candidate:
+        if sum(c) == S:
+            answer.append(c)
 
-    if sum(x) == S and x not in answer:
-        answer.add(x)
-        continue
-
-    for n in nums:
-        if n not in x:
-            q.append(x + [n])
 
 print(len(answer))
