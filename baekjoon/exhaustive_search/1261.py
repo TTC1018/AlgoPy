082fa1eea914ac086a0666c911e293eb2b94430e
DongHyeon Park <paramore@kakao.com>
2021-08-11 16:19:19 +0900

    Update 1261.py

--- baekjoon/exhaustive_search/1261.py
+++ baekjoon/exhaustive_search/1261.py
@@ -7,26 +7,31 @@ for i in range(N):
     room.append(list(map(int, list(input()))))
 
 # 상하좌우
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [1, 0, 0, -1]
+dy = [0, 1, -1, 0]
 
 q = deque()
 q.append((0, 0, 0))
 
+visited = [[False] * M for _ in range(N)]
+visited[0][0] = True
 answer = (N-1) + (M-1) - 1
+
 while q:
     x, y, count = q.popleft()
 
     if x == M-1 and y == N-1:
-        answer = min(answer, count)
-        continue
+        answer = count
+        break
 
     for i in range(4):
         new_x = x + dx[i]
         new_y = y + dy[i]
         if 0 <= new_x < M and 0 <= new_y < N:
-            if room[new_y][new_x] == 1:
-                q.append((new_x, new_y, count + 1))
-            else:
-                q.append((new_x, new_y, count))
+            if not visited[new_y][new_x]:
+                visited[new_y][new_x] = True
+                if room[new_y][new_x] == 0:
+                    q.appendleft((new_x, new_y, count)) # 저비용 우선 탐색
+                else:
+                    q.append((new_x, new_y, count + 1))
 print(answer)
