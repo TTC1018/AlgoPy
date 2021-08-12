721bc31bc1caa215dadea083b46507c3dc5dcb33
DongHyeon Park <paramore@kakao.com>
2021-08-12 17:53:24 +0900

    Update 1208.py

--- baekjoon/exhaustive_search/1208.py
+++ baekjoon/exhaustive_search/1208.py
@@ -1,20 +1,29 @@
+from collections import deque
+
 N, S = map(int, input().split(' '))
 nums = list(map(int, input().split(' ')))
 
-left, right = 0, 1
+q = deque()
+q.append((0, len(nums)))
+
 answer = 0
+while q:
+    l, r = q.popleft()
 
-abs_S = abs(S)
-while left <= right <= N:
-    temp_sum = abs(sum(nums[left:right]))
-    if temp_sum == abs_S:
-        print(left, right)
+    temp_sum = sum(nums[l:r])
+    if temp_sum == S:
         answer += 1
+        continue
 
-    if temp_sum < abs_S:
-        right += 1
-    else:
-        left += 1
+    n_left = l + 1
+    if n_left < r < len(nums):
+        q.append((n_left, r))
+
+    n_right = r - 1
+    if 0 < l < n_right:
+        q.append((l, n_right))
+
+
 
 print(answer)
 
