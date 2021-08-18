d14afda9e960fdf7306bdac7a8f4cf34ebe2527a
DongHyeon Park <paramore@kakao.com>
2021-08-18 21:03:32 +0900

    Update 2143.py

--- baekjoon/binary_search/2143.py
+++ baekjoon/binary_search/2143.py
@@ -1,18 +1,20 @@
+# defaultdict는 
+
 T = int(input())
 n = int(input())
 A = list(map(int, input().split(' ')))
 m = int(input())
 B = list(map(int, input().split(' ')))
 
-sum_A, sum_B = [0] * 2000000001, [0] * 2000000001
+sum_A, sum_B = {}, {}
 for i in range(1, n + 1):
     for j in range(n - i + 1):
-        sum_A[sum(A[j:j+i]) + 1000000000] += 1
+        sum_A[sum(A[j:j+i])] = sum_A.get(sum(A[j:j+i]), 0) + 1
 for i in range(1, m + 1):
     for j in range(m - i + 1):
-        sum_B[sum(B[j:j+i]) + 1000000000] += 1
+        sum_B[sum(B[j:j+i])] = sum_B.get(sum(B[j:j+i]), 0) + 1
 
 answer = 0
-for i in range(1, T):
-        answer += (sum_A[i + 1000000000] * sum_B[T-i + 1000000000])
+for val_a in sum_A:
+    answer += (sum_A[val_a] * sum_B.get(T-val_a, 0))
 print(answer)
