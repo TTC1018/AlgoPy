3cc3e60b92ed9c80ee006fc1c3bce43b81db4355
DongHyeon Park <paramore@kakao.com>
2021-08-19 17:35:49 +0900

    Update 12100.py

--- baekjoon/exhaustive_search/12100.py
+++ baekjoon/exhaustive_search/12100.py
@@ -1,4 +1,5 @@
 from collections import deque
+from copy import deepcopy
 
 N = int(input())
 B = []
@@ -9,14 +10,78 @@ for i in range(N):
 q = deque()
 q.append((B, 0))
 
+answer = 0
 while q:
     temp_B, count = q.popleft()
+    if count == 5:
+        for l in temp_B:
+            answer = max(answer, max(l))
+        continue
 
-    #상
-    for i in range(N):
-        
-    #하
+    # 상
+    up_B = [[0] * N for _ in range(N)]
+    for i in range(N):  # 열
+        visited = False
+        col_B = [l[i] for l in temp_B]
+        cursor = 1
+        while cursor < len(col_B):  # 행
+            if col_B[cursor] != 0 and col_B[cursor] == col_B[cursor - 1] and not visited:
+                visited = True
+                col_B[cursor] *= 2
+                col_B.pop(cursor - 1)
+                col_B.append(0)
+            else:
+                visited = False
+                cursor += 1
+        for j in range(N):
+            up_B[j][i] = col_B[j]
+    if temp_B != up_B:
+        q.append((up_B, count + 1))
+    # 하
 
-    #좌
+    # 좌
+    lf_B = [[0] * N for _ in range(N)]
+    for i in range(N):  # 행
+        visited = False
+        row_B = deepcopy(temp_B[i])
+        cursor = 1
+        while cursor < len(row_B):  # 렬
+            if row_B[cursor] != 0 and row_B[cursor] == row_B[cursor - 1] and not visited:
+                visited = True
+                row_B[cursor] *= 2
+                row_B.pop(cursor - 1)
+                row_B.append(0)
+            else:
+                visited = False
+                cursor += 1
+        for j in range(N):
+            lf_B[i][j] = row_B[j]
+    if temp_B != lf_B:
+        q.append((lf_B, count + 1))
+    # 우
+    rt_B = [[0] * N for _ in range(N)]
+    for i in range(N):  # 행
+        visited = False
+        row_B = deepcopy(temp_B[i])
+        cursor = -1
+        while abs(cursor) < len(row_B):  # 렬
+            if row_B[cursor] == 0:
+                visited = False
+                row_B[cursor] = row_B[cursor - 1]
+                row_B.pop(cursor - 1)
+                row_B.insert(0, 0)
+            elif row_B[cursor] == row_B[cursor - 1] and not visited:
+                visited = True
+                row_B[cursor] *= 2
+                row_B.pop(cursor - 1)
+                row_B.insert(0, 0)
+            else:
+                visited = False
+                cursor -= 1
+        for j in range(N):
+            rt_B[i][j] = row_B[j]
+    print(temp_B, rt_B)
+    if temp_B != rt_B:
+        q.append((rt_B, count + 1))
 
-    #우
+print(answer)
