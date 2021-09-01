3fce44e9629fce42bbeb813ababc1ee52ad6b96a
DongHyeon Park <paramore@kakao.com>
2021-09-01 16:03:44 +0900

    Update 17142.py

--- baekjoon/exhaustive_search/17142.py
+++ baekjoon/exhaustive_search/17142.py
@@ -1,58 +1,60 @@
+import sys
 from itertools import combinations
 from copy import deepcopy
 from collections import deque
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-def spread(r_var, row, col, cnt):
-    for i in range(4):
-        nr = row + dx[i]
-        nc = col + dy[i]
-        if 0 <= nr < N and 0 <= nc < N:
-            if r_var[nr][nc] == 0 or r_var[nr][nc] <= cnt - 1:
-                r_var[nr][nc] = cnt - 1
-                spread(r_var, nr, nc, cnt - 1)
-
-
-def check(room_map):
-    for r in room_map:
-        if 0 in r:
-            return False
-    return True
-
-
 N, M = map(int, input().split())
 rooms = []
 vs = []
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
 
 for i in range(N):
     rooms.append(list(map(int, input().split())))
     for j in range(N):
         if rooms[i][j] == 2:
-            vs.append((i, j))
+            vs.append((i, j, 0))
 
 
-answer = -(N + 1)
-q = deque()
+def check(room_map):
+    for i in range(N):
+        for j in range(N):
+            if room_map[i][j] == 0:
+                return False
+    return True
+
+
+def spread(points, room_map):
+    visited = [[False] * N for _ in range(N)]
+    temp_map = deepcopy(room_map)
+    q = deque()
+
+    q.extend(points)
+    result = 0
+    while q:
+        r, c, cnt = q.popleft()
+        visited[r][c] = True
+        for i in range(4):
+            nr, nc = r + dx[i], c + dy[i]
+            if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc] and temp_map[nr][nc] != 1:
+                visited[nr][nc] = True
+                if temp_map[nr][nc] == 0:
+                    temp_map[nr][nc] = 2
+                    result = cnt + 1
+                q.append((nr, nc, cnt + 1))
+    if check(temp_map):
+        return result
+    else:
+        return -1
+
+
+answer = sys.maxsize
 for c in combinations(vs, M):
-    q.append(c)
+    result = spread(c, rooms)
+    if result != -1:
+        answer = min(answer, result)
 
-while q:
-    temp_rooms = deepcopy(rooms)
-    for virus in q.popleft():
-        row, col = virus
-        spread(temp_rooms, row, col, 0)
-    if check(temp_rooms):
-        c_answer = min(temp_rooms[0])
-        for i in range(1, N):
-            c_answer = min(c_answer, min(temp_rooms[i]))
-        answer = max(answer, c_answer)
-
-if answer == -(N + 1):
+if answer == sys.maxsize:
     print(-1)
-elif answer == 1 or answer == 2:
-    print(0)
 else:
-    print(abs(answer))
+    print(answer)
