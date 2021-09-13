25f978d6b52e9492788e0d102960e02e1a8a1043
DongHyeon Park <paramore@kakao.com>
2021-09-13 17:22:22 +0900

    Update 17135.py

--- baekjoon/exhaustive_search/17135.py
+++ baekjoon/exhaustive_search/17135.py
@@ -1,48 +1,96 @@
-from itertools import combinations
+from collections import deque
 from copy import deepcopy
 
-N, M, D = map(int, input().split()) # 행 열 사정거리
-grids = []
-break_condition = [[0] * M for _ in range(N)]
-for _ in range(N):
-    grids.append(list(map(int, input().split())))
+N, M, D = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(N)]
+data.append([0] * M)
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+# 궁수가 공격하는 적은 거리가 D 이하인 적 중 가장 가까운 적
 
-# 궁수 3명 고정 (위치는 자유)
-# 사정거리 내에 여러 대상이 있다면 가장 왼쪽을 사격
-# 두 좌표 간의 거리 = |x1 - x2| + |y1 - y2|
-# 적은 가장 아래 행을 넘어서면 소멸
-# 제거 가능한 최대수
-def solve(rangers):
-    grid_copy = deepcopy(grids)
-    result = set()
-    while grid_copy != break_condition:
-        for ranger in rangers:
-            flag = False
-            for i in range(ranger - (D - 1), ranger + (D - 1) + 1):
-                if flag:
-                    break
-                if 0 <= i < M:
-                    for j in range(D - abs(ranger - i)):
-                        row = (N - 1) - j
-                        if 0 <= row < N:
-                            if grid_copy[row][i] == 1:
-                                continueFlag = False
-                                for k in range(row):
-                                    if 1 in grid_copy[k][i] and (row, i) in result:
-                                        continueFlag = True
-                                        break
-                                if continueFlag:
-                                    continue
-                                else:
-                                    result.add((row, i))
-                                    flag = True
-                                break
-        grid_copy.pop(N - 1)
-        grid_copy.insert(0, [0] * M)
-    return len(list(result))
+# 적이 하나도 없는지 확인 (있으면 False 리턴)
+def check(data):
+    for i in range(N):
+        for j in range(M):
+            if data[i][j] == 1:
+                return False
+    return True
+
+
+# 거리가 D이하인 적 중에서 가장 가까운 적 위치 반환
+def bfs(x, y, data):
+    q = deque()
+    q.append((x, y))
+    check = {(x, y): 1}
+    point = []
+    nq = deque()
+    distance = 0
+    while 1:
+        while q:
+            x, y = q.popleft()
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                if 0 <= nx < N and 0 <= ny < M and check.get((nx, ny)) is None:
+                    check[(nx, ny)] = 1
+                    if data[nx][ny] == 1:
+                        point.append((nx, ny))
+                    else:
+                        nq.append((nx, ny))
+
+        distance += 1
+        if distance > D:
+            return -1, -1
+
+        if point:
+            if len(point) > 1:
+                point = sorted(point, key=lambda x: x[1])
+            return point[0][0], point[0][1]
+        else:
+            q = nq
+            nq = deque()
+
+
+# 제거한 적의 개수 반환
+def remove_enemy_count(data):
+    count = 0
+    while 1:
+        # 공격하고
+        enemy = set()
+        for j in range(M):
+            if data[N][j] == 2:
+                e_x, e_y = bfs(N, j, data)
+                if e_x != -1:
+                    enemy.add((e_x, e_y))
+        count += len(enemy)
+        for x, y in enemy:
+            data[x][y] = 0
+
+        # 적이 이동
+        for i in range(N - 1, -1, -1):
+            for j in range(M):
+                if data[i][j] == 1:
+                    data[i][j] = 0
+                    if i != (N - 1):
+                        data[i + 1][j] = 1
+        if check(data):
+            return count
 
 
 answer = 0
-for c in combinations([n for n in range(M)], 3):
-    answer = max(answer, solve(c))
+# 궁수 3명 배치 함수
+def solve(count, data):
+    global answer
+    if count == 3:
+        data = deepcopy(data)
+        answer = max(answer, remove_enemy_count(data))
+        return
+
+    for i in range(M):
+        if data[N][i] == 0:
+            data[N][i] = 2
+            solve(count + 1, data)
+            data[N][i] = 0
+
+
+solve(0, data)
 print(answer)
