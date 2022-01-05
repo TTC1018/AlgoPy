cb5709b5d03210bb0255cf681ac393055e73e703
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2022-01-06 01:07:33 +0900

    Update 4_1_1.py

--- coding_test/9hyun/4_1_1.py
+++ coding_test/9hyun/4_1_1.py
@@ -2,13 +2,13 @@ N = int(input())
 routes = input().split(' ')
 
 start = (1, 1)
-d = {'L':(0, -1), 'R':(0, 1), 'U':(-1, 0), 'D':(1, 0)}
+d = {'L': (0, -1), 'R': (0, 1), 'U': (-1, 0), 'D': (1, 0)}
 
 for r in routes:
     n_x = start[0] + d[r][0]
     n_y = start[1] + d[r][1]
 
-    if 1 <= n_x <= 5 and 1<= n_y <= 5:
+    if 1 <= n_x <= 5 and 1 <= n_y <= 5:
         start = (n_x, n_y)
 
 print(start[0], start[1])
