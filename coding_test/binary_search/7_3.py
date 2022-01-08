1b1cdd3bba87bbd536858034a4a51a9957bac90f
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2022-01-09 01:46:31 +0900

    7-[3]실전문제

---
+++ coding_test/binary_search/7_3.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+dduck = list(map(int, input().split()))
+
+start, end = 0, max(dduck)
+answer = 0
+while start <= end:
+    t_sum = 0
+    mid = (start + end) // 2
+    for dd in dduck:
+        if dd > mid:
+            t_sum += dd - mid
+
+    if t_sum >= M:
+        answer = mid
+        start = mid + 1
+    elif t_sum < M:
+        end = mid -1
+
+print(answer)
