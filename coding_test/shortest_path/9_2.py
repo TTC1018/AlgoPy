dab277aefda653cec28a24d8995cec6e69ab5839
DongHyeon Park <paramore@kakao.com>
2022-01-10 17:55:45 +0900

    플로이드 워셜

--- coding_test/shortest_path/9_2.py
+++ coding_test/shortest_path/9_2.py
@@ -7,12 +7,12 @@ INF = int(1e9)
 N, M = map(int, input().split())
 
 start = int(input())
-graph = [[] for i in range(N + 1)]
-distance = [INF] * (N + 1)
+graph = [[] for i in range(N + 1)] # 경로 그래프
+distance = [INF] * (N + 1) # 최단거리 값 저장 리스트
 
 for _ in range(M):
     a, b, c = map(int, input().split())
-    graph[a].append((b, c))
+    graph[a].append((b, c)) # a -> b 의 거리값이 c임을 의미
 
 def dijkstra(start):
     q = []
@@ -21,12 +21,12 @@ def dijkstra(start):
     distance[start] = 0
     while q:
         dist, now = heapq.heappop(q)
-        if distance[now] < dist:
+        if distance[now] < dist: # 이미 방문했거나 (최단거리값이 INF가 아니거나) 계산해봤자 최단거리가 아닌 경우
             continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
+        for i in graph[now]: # 노드now와 연결된 노드 간의 거리 순차 확인
+            cost = dist + i[1] # 새로운 거리
+            if cost < distance[i[0]]: # 기존 최단거리보다 짧을 경우
+                distance[i[0]] = cost # 최단거리 교체
                 heapq.heappush(q, (cost, i[0]))
 
 dijkstra(start)
