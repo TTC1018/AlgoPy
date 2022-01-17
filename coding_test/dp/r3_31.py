ac3eaa1bbfbeafaa2ad358b127324f63e8087353
DongHyeon Park <paramore@kakao.com>
2022-01-17 16:09:40 +0900

    PART3 DP 31

---
+++ coding_test/dp/r3_31.py
@@ -0,0 +1,23 @@
+from copy import deepcopy
+
+T = int(input())
+direction = [(-1, -1), (0, -1), (1, -1)]
+
+for i in range(T):
+    n, m = map(int, input().split())
+    golds = list(map(int, input().split()))
+    dp = deepcopy(golds)
+
+    for j in range(1, m):
+        for i in range(n):
+            for d in direction:
+                nx, ny = i + d[0], j + d[1]
+                if 0 <= nx < n and 0 <= ny < m:
+                    dp[i * m + j] = max(dp[i * m + j], golds[i * m + j] + dp[nx * m + ny])
+
+    answer = 0
+    for i in range(n):
+        answer = max(answer, dp[i * m + m - 1])
+    print(answer)
+
+
