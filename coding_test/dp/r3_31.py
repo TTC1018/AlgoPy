443eeb5e4863d51f4f670b1cc0b3790464bc11bb
DongHyeon Park <paramore@kakao.com>
2022-01-21 16:30:06 +0900

    Update r3_31.py

--- coding_test/dp/r3_31.py
+++ coding_test/dp/r3_31.py
@@ -10,14 +10,14 @@ for i in range(T):
 
     for j in range(1, m):
         for i in range(n):
-            for d in direction:
+            for d in direction: # 현재 위치로 올 수 있는 경로들
                 nx, ny = i + d[0], j + d[1]
-                if 0 <= nx < n and 0 <= ny < m:
-                    dp[i * m + j] = max(dp[i * m + j], golds[i * m + j] + dp[nx * m + ny])
+                if 0 <= nx < n and 0 <= ny < m: # 가능한 이전 위치인지 확인
+                    dp[i * m + j] = max(dp[i * m + j], golds[i * m + j] + dp[nx * m + ny]) # 가능한 경로들 중에 최대값을 선택
 
     answer = 0
     for i in range(n):
-        answer = max(answer, dp[i * m + m - 1])
+        answer = max(answer, dp[i * m + m - 1]) # 열 끝에 있는 칸이 최종 지점들의 후보이므로 하나씩 확인
     print(answer)
 
 
