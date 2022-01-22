89afab2ae836042e5579bb1b67be9d68a9af6b91
DongHyeon Park <paramore@kakao.com>
2022-01-22 21:05:46 +0900

    Update 2108.py

--- baekjoon/sorting/2108.py
+++ baekjoon/sorting/2108.py
@@ -13,7 +13,7 @@ nums.sort()
 print(round(sum(nums)/N))
 print(nums[N // 2])
 
-counts = Counter(nums).most_common()
+counts = Counter(nums).most_common() # most_common을 쓰면 리스트에 튜플 형태 오름차순으로 담아줌
 if N > 1:
     if counts[0][1] == counts[1][1]:
         print(counts[1][0])
