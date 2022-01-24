a7c9c165a416e7f216fe812f06f0948b644ae86d
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2022-01-24 21:03:05 +0900

    Update

--- baekjoon/greedy/1459.py
+++ baekjoon/greedy/1459.py
@@ -1,12 +1,11 @@
-INF = 1e9
-
 X, Y, W, S = map(int, input().split())
 
 bigger, smaller = max(X, Y), min(X, Y)
+isOdd = (X + Y) % 2
 
-answer = 0
-if 2 * W > S:
-    answer = (bigger // 2) * 2 * S + (bigger % 2) * W
-else:
-    answer = (X + Y) * W
+a1 = (X + Y) * W # 평행 이동만
+a2 = smaller * S + (bigger - smaller) * W # 대각선 + 평행이동
+a3 = (bigger - isOdd) * S + isOdd * W # 대각선 최대한 많이 사용하는 경우
+answer = min(a1, a2, a3)
+
 print(answer)
