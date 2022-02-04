633385a3768f65d95106a253d0a57dd96bbf4729
DongHyeon Park <paramore@kakao.com>
2022-02-04 12:44:46 +0900

    Update r3_3.py

--- coding_test/greedy/r3_3.py
+++ coding_test/greedy/r3_3.py
@@ -3,9 +3,5 @@ S = input()
 one_list = [s for s in S.split('0') if s != '']
 zero_list = [s for s in S.split('1') if s != '']
 
-answer = 0
-if len(one_list) < len(zero_list):
-    answer = len(one_list)
-else:
-    answer = len(zero_list)
+answer = min(len(one_list), len(zero_list))
 print(answer)
