b129dee3dadb3165539b051841a60376645b041c
DongHyeon Park <paramore@kakao.com>
2022-02-04 16:01:50 +0900

    PART 3 최단경로 38
    플로이드 워셜

--- coding_test/shortest_path/r3_38.py
+++ coding_test/shortest_path/r3_38.py
@@ -1,26 +1,25 @@
-def find_child(node):
-
-    if len(child_candidate[node]) == 0:
-        return 1
-    else:
-        for child in child_candidate[node]:
-            child_count[node] += find_child(child)
-
+INF = int(1e9)
 
 N, M = map(int, input().split())
-child_count = [0] * (N + 1)
-child_candidate = [[] for _ in range(N + 1)]
-parent_candidate = [[] for _ in range(N + 1)]
-for i in range(M):
-    x, y = map(int, input().split())
-    parent_candidate[x].append(y)
-    child_candidate[y].append(x)
+graph = [[INF] * (N + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    graph[i][i] = 0
 
-print(child_count)
+for _ in range(M):
+    x, y = map(int, input().split())
+    graph[x][y] = 1
+
+for k in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
 answer = 0
 for i in range(1, N + 1):
-    find_child(i)
-    if child_count[i] + len(parent_candidate[i]) == N:
+    count = 0
+    for j in range(1, N + 1):
+        if graph[i][j] != INF or graph[j][i] != INF: # i < j 이거나 i > j인 경우 (비교 가능한 경우)
+            count += 1
+    if count == N: # 모든 수와 비교가 가능하면 답이 될 수 있음
         answer += 1
 print(answer)
