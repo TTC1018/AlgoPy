ae31d9ed3820c577fdd275b2c3338688966ac410
DongHyeon Park <paramore@kakao.com>
2022-02-10 20:54:56 +0900

    PART 3 BFS/DFS 17
    BFS로 풀이

--- coding_test/dfs_bfs/r3_17.py
+++ coding_test/dfs_bfs/r3_17.py
@@ -1,3 +1,36 @@
+from collections import deque
+
 N, K = map(int, input().split())
-graph = [list(map(int, input())) for _ in range(N)]
-S, X, Y = map(int, input())
+q = deque()
+
+graph = []
+for i in range(N):
+    data = list(map(int, input().split()))
+    for j in range(N):
+        if data[j] != 0:
+            q.append((data[j], i, j))
+    graph.append(data)
+
+S, X, Y = map(int, input().split())
+q = deque(sorted(q, key=lambda x:x[0]))
+
+direc = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+second, t_sec = 0, len(q)
+while q:
+    if second == S:
+        break
+
+    virus, x, y = q.popleft()
+    for d in direc:
+        nx, ny = x + d[0], y + d[1]
+        if 0 <= nx < N and 0 <= ny < N:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = virus
+                q.append((virus, nx, ny))
+    t_sec -= 1
+
+    if t_sec == 0:
+        second += 1
+        t_sec = len(q)
+
+print(graph[X - 1][Y - 1])
