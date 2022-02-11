55f69dc6d163f38cb4593a83b6b1410bc6947fd3
DongHyeon Park <paramore@kakao.com>
2022-02-11 14:41:05 +0900

    주석 추가

--- coding_test/binary_search/r3_29.py
+++ coding_test/binary_search/r3_29.py
@@ -2,14 +2,15 @@ N, C = map(int, input().split())
 houses = [int(input()) for _ in range(N)]
 houses.sort()
 
-start, end = 1, houses[-1] - houses[0]
+start, end = 1, houses[-1] - houses[0] # 가능한 최소갭 & 최대갭
 answer = 0
 while start <= end:
     mid = (start + end) // 2
-    point = houses[0]
+    point = houses[0] # 첫 집부터 확인
     set_count = 1
+
     for i in range(1, N):
-        if houses[i] >= point + mid:
+        if houses[i] >= point + mid: # 현재 집에서 gap 만큼 간 거리보다 다음 집이 멀 때 (목표거리 충족)
             point = houses[i]
             set_count += 1
 
--- coding_test/dfs_bfs/r3_17.py
+++ coding_test/dfs_bfs/r3_17.py
@@ -1,36 +1,36 @@
 from collections import deque
 
 N, K = map(int, input().split())
-q = deque()
+q = deque() # BFS 풀이
 
 graph = []
 for i in range(N):
     data = list(map(int, input().split()))
     for j in range(N):
         if data[j] != 0:
-            q.append((data[j], i, j))
+            q.append((data[j], i, j)) # 바이러스가 있는 위치를 기록
     graph.append(data)
 
 S, X, Y = map(int, input().split())
-q = deque(sorted(q, key=lambda x:x[0]))
+q = deque(sorted(q, key=lambda x:x[0])) # 숫자가 낮은 바이러스부터 시작해야 되므로 정렬
 
-direc = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-second, t_sec = 0, len(q)
+direc = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 상하좌우
+second, t_sec = 0, len(q) # 1초가 지났는 지를 확인 하려면 현재 큐의 길이를 초를 세는 단위로 이용하면 된다
 while q:
     if second == S:
         break
 
     virus, x, y = q.popleft()
     for d in direc:
-        nx, ny = x + d[0], y + d[1]
+        nx, ny = x + d[0], y + d[1] # 새로운 위치
         if 0 <= nx < N and 0 <= ny < N:
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = virus
-                q.append((virus, nx, ny))
+            if graph[nx][ny] == 0: # 바이러스를 퍼뜨릴 수 있는 위치
+                graph[nx][ny] = virus # 바이러스 퍼뜨리기
+                q.append((virus, nx, ny)) # 큐에 등록
     t_sec -= 1
 
-    if t_sec == 0:
+    if t_sec == 0: # 해당 분기가 끝났을 때가 1초가 지난 것
         second += 1
-        t_sec = len(q)
+        t_sec = len(q) # 새로운 분기가 시작됐음을 기록
 
 print(graph[X - 1][Y - 1])
