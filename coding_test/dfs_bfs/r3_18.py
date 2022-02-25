4f53e3a80918e51f4b4610034e96d08b4cc70871
DongHyeon Park <paramore@kakao.com>
2022-02-25 23:18:20 +0900

    PART 3 DFS/BFS 18

--- coding_test/dfs_bfs/r3_18.py
+++ coding_test/dfs_bfs/r3_18.py
@@ -10,14 +10,40 @@ def check_complete(strs):
     return True
 
 
+def find_balance(strs):
+    left = 0
+    for i in range(len(strs)):
+        if strs[i] == '(':
+            left += 1
+        else:
+            left -= 1
 
-
+        if left == 0:
+            return i
 
 
 def solution(p):
     answer = ''
+    if p == '':
+        return answer
 
+    split = find_balance(p)
+    u, v = p[:split + 1], p[split + 1:]
 
+    if check_complete(u):
+        answer = u + solution(v)
+    else:
+        answer = '(' + solution(v) + ')'
+        u = list(u[1:-1])
 
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        answer += ''.join(u)
 
     return answer
+
+
+solution(')(')
