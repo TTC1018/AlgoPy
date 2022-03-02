981f589fe088db4f756c068528f3fb0c1ca65bd3
DongHyeon Park <paramore@kakao.com>
2022-03-02 21:53:43 +0900

    Update r3_30.py

--- coding_test/binary_search/r3_30.py
+++ coding_test/binary_search/r3_30.py
@@ -1,40 +1,96 @@
+from bisect import bisect_left, bisect_right
+
+
+def l_q_search(q):
+    l_qm = 0
+    for i in range(len(q)):
+        if q[i] != '?':
+            break
+        l_qm += 1
+    return l_qm
+
+
+def r_q_search(q):
+    r_qm = len(q) - 1
+    for i in range(len(q) - 1, -1, -1):
+        if q[i] != '?':
+            break
+        r_qm -= 1
+    return r_qm
+
 # words = 가사의 모든 단어들 (2개 이상)
 # queries = 찾고자 하는 키워드 (2개 이상)
 def solution(words, queries):
     answer = []
     w_size = len(words)
 
-    words.sort(key=lambda  x:len(x))
+    words.sort(key=lambda x: (len(x), x))
     for q in queries:
         q_len = len(q)
-        l_qm, r_qm = 0, q_len - 1
-        # 좌우 '?' 얼마나 있는지 확인
-        for i in range(q_len):
-            if q[i] != '?':
-                break
-            l_qm += 1
-        for i in range(q_len, -1, -1):
-            if q[i] != '?':
-                break
-            r_qm -= 1
+        l_qm, r_qm = l_q_search(q), r_q_search(q)
 
-        count = 0
+        temp_answer = set()
         start, end = 0, w_size - 1
-        mid = (start + end) // 2
         while start <= end:
+            mid = (start + end) // 2
             t_len = len(words[mid])
-            if t_len > q_len:
+            if t_len > q_len:  # 대상 길이가 쿼리보다 긴 경우
                 end = mid - 1
-                continue
-            elif t_len < q_len:
+            elif t_len < q_len:  # 대상 길이가 쿼리보다 짧은 경우
                 start = mid + 1
-                continue
             else:
-                for i in range(len(words[mid])):
-                    if w ==
+                if words[mid][l_qm:r_qm + 1] == q[l_qm:r_qm + 1]:
+                    temp_answer.add(words[mid])
+                    start = mid + 1
+                else:
+                    end = mid - 1
 
+        start, end = 0, w_size - 1
+        while start <= end:
+            mid = (start + end) // 2
+            t_len = len(words[mid])
+            if t_len > q_len:  # 대상 길이가 쿼리보다 긴 경우
+                end = mid - 1
+            elif t_len < q_len:  # 대상 길이가 쿼리보다 짧은 경우
+                start = mid + 1
+            else:
+                if words[mid][l_qm:r_qm + 1] == q[l_qm:r_qm + 1]:
+                    temp_answer.add(words[mid])
+                    end = mid - 1
+                else:
+                    start = mid + 1
 
-        answer.append(count)
+        start, end = 0, w_size - 1
+        while start <= end:
+            mid = (start + end) // 2
+            t_len = len(words[mid])
+            if t_len > q_len:  # 대상 길이가 쿼리보다 긴 경우
+                end = mid - 1
+            elif t_len < q_len:  # 대상 길이가 쿼리보다 짧은 경우
+                start = mid + 1
+            else:
+                if words[mid][l_qm:r_qm + 1] == q[l_qm:r_qm + 1]:
+                    temp_answer.add(words[mid])
+                end = mid - 1
 
+        start, end = 0, w_size - 1
+        while start <= end:
+            mid = (start + end) // 2
+            t_len = len(words[mid])
+            if t_len > q_len:  # 대상 길이가 쿼리보다 긴 경우
+                end = mid - 1
+            elif t_len < q_len:  # 대상 길이가 쿼리보다 짧은 경우
+                start = mid + 1
+            else:
+                if words[mid][l_qm:r_qm + 1] == q[l_qm:r_qm + 1]:
+                    temp_answer.add(words[mid])
+                start = mid + 1
+
+        answer.append(len(temp_answer))
 
     return answer
+
+
+result = solution(["frodo", "front", "frost", "frozen", "frame", "kakao"],
+                  ["fro??", "????o", "fr???", "fro???", "pro?"])
+print(result)
