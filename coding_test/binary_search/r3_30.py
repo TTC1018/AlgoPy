d090f1a62eb573a474ad59df9820285f04bdb2ac
DongHyeon Park <paramore@kakao.com>
2022-03-03 16:28:22 +0900

    PART 3 이진탐색 30 solved

--- coding_test/binary_search/r3_30.py
+++ coding_test/binary_search/r3_30.py
@@ -1,103 +1,32 @@
 from bisect import bisect_left, bisect_right
 
 
-def l_q_search(q):
-    l_qm = 0
-    for i in range(len(q)):
-        if q[i] != '?':
-            break
-        l_qm += 1
-    return l_qm
+def word_count(words, l_standard, r_standard):
+    left = bisect_left(words, l_standard)
+    right = bisect_right(words, r_standard)
+    return right - left
 
-
-def r_q_search(q):
-    r_qm = len(q) - 1
-    for i in range(len(q) - 1, -1, -1):
-        if q[i] != '?':
-            break
-        r_qm -= 1
-    return r_qm
 
 # words = 가사의 모든 단어들 (2개 이상)
 # queries = 찾고자 하는 키워드 (2개 이상)
 def solution(words, queries):
     answer = []
-    word_arr = [[] for _ in range(10001)]
-    word_arr_rev = [[] for _ in range(10001)]
 
-    for w in words:
-        word_arr[len(w)].append(w)
-        word_arr_rev[len(w)].append(w[::-1])
+    max_len = len(sorted(queries, key=lambda x:len(x))[-1]) # 제일 긴 쿼리 길이 확인
 
-    w_size = len(words)
+    word_arr = [[w for w in words if len(w) == i] for i in range(max_len + 1)]
+    word_arr_rev = [[w[::-1] for w in words if len(w) == i] for i in range(max_len + 1)] # bisect를 이용할 것이기 때문에, ?가 앞쪽에 있는 쿼리들은 뒤집은 뒤 기존 사용법을 적용
 
-    words.sort(key=lambda x: (len(x), x))
+    for i in range(max_len + 1):
+        word_arr[i].sort()
+        word_arr_rev[i].sort()
+
     for q in queries:
         q_len = len(q)
-        l_qm, r_qm = l_q_search(q), r_q_search(q)
 
-        temp_answer = set()
-        start, end = 0, w_size - 1
-        while start <= end:
-            mid = (start + end) // 2
-            t_len = len(words[mid])
-            if t_len > q_len:  # 대상 길이가 쿼리보다 긴 경우
-                end = mid - 1
-            elif t_len < q_len:  # 대상 길이가 쿼리보다 짧은 경우
-                start = mid + 1
-            else:
-                if words[mid][l_qm:r_qm + 1] == q[l_qm:r_qm + 1]:
-                    temp_answer.add(words[mid])
-                    start = mid + 1
-                else:
-                    end = mid - 1
-
-        start, end = 0, w_size - 1
-        while start <= end:
-            mid = (start + end) // 2
-            t_len = len(words[mid])
-            if t_len > q_len:  # 대상 길이가 쿼리보다 긴 경우
-                end = mid - 1
-            elif t_len < q_len:  # 대상 길이가 쿼리보다 짧은 경우
-                start = mid + 1
-            else:
-                if words[mid][l_qm:r_qm + 1] == q[l_qm:r_qm + 1]:
-                    temp_answer.add(words[mid])
-                    end = mid - 1
-                else:
-                    start = mid + 1
-
-        start, end = 0, w_size - 1
-        while start <= end:
-            mid = (start + end) // 2
-            t_len = len(words[mid])
-            if t_len > q_len:  # 대상 길이가 쿼리보다 긴 경우
-                end = mid - 1
-            elif t_len < q_len:  # 대상 길이가 쿼리보다 짧은 경우
-                start = mid + 1
-            else:
-                if words[mid][l_qm:r_qm + 1] == q[l_qm:r_qm + 1]:
-                    temp_answer.add(words[mid])
-                end = mid - 1
-
-        start, end = 0, w_size - 1
-        while start <= end:
-            mid = (start + end) // 2
-            t_len = len(words[mid])
-            if t_len > q_len:  # 대상 길이가 쿼리보다 긴 경우
-                end = mid - 1
-            elif t_len < q_len:  # 대상 길이가 쿼리보다 짧은 경우
-                start = mid + 1
-            else:
-                if words[mid][l_qm:r_qm + 1] == q[l_qm:r_qm + 1]:
-                    temp_answer.add(words[mid])
-                start = mid + 1
-
-        answer.append(len(temp_answer))
+        if q[0] == '?':
+            answer.append(word_count(word_arr_rev[q_len], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z')))
+        else:
+            answer.append(word_count(word_arr[q_len], q.replace('?', 'a'), q.replace('?', 'z')))
 
     return answer
-
-
-result = solution(["frodo", "front", "frost", "frozen", "frame", "kakao"],
-                  ["fro??", "????o", "fr???", "fro???", "pro?"])
-print(result)
