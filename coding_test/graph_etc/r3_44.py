0777df062cd46f00dd85057d76ab0533b1730eb7
DongHyeon Park <paramore@kakao.com>
2022-03-04 16:56:23 +0900

    Update r3_44.py

--- coding_test/graph_etc/r3_44.py
+++ coding_test/graph_etc/r3_44.py
@@ -24,13 +24,13 @@ for i in range(N):
     points[1].append((Y, i))
     points[2].append((Z, i))
 
-points[0].sort(), points[1].sort(), points[2].sort()
+points[0].sort(), points[1].sort(), points[2].sort() # X, Y, Z 축 좌표 각각 정렬
 edges = []
 for i in range(N - 1):
-    edges.append((points[0][i + 1][0] - points[0][i][0], points[0][i][1], points[0][i + 1][1]))
+    edges.append((points[0][i + 1][0] - points[0][i][0], points[0][i][1], points[0][i + 1][1])) # 정렬된 좌표간의 거리 = X축만 고려했을 때 최솟값을 가지는 간선들
     edges.append((points[1][i + 1][0] - points[1][i][0], points[1][i][1], points[1][i + 1][1]))
     edges.append((points[2][i + 1][0] - points[2][i][0], points[2][i][1], points[2][i + 1][1]))
-edges.sort()
+edges.sort() # X/Y/Z 각각의 축만 고려한 간선값 후보라 중복되는 간선이 있을 수도 있음, 그러나 간선 값 기준으로 정렬 후에 크루스칼 알고리즘으로 사용하면 중복 선택이 예방됨
 
 answer = 0
 for e in edges:
