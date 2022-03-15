3852d3f937970657b2852401c4d7700f713e90e7
DongHyeon Park <paramore@kakao.com>
2022-03-15 15:34:58 +0900

    PART 3 그리디 6

--- coding_test/greedy/r3_6.py
+++ coding_test/greedy/r3_6.py
@@ -1,24 +1,28 @@
+import heapq
+
+
 def solution(food_times, k):
+    answer = -1
 
-    answer = 0
-    sec, f_len = 0, len(food_times)
+    q = []
+    f_len = len(food_times)
     for i in range(f_len):
-        food_times[i] *= f_len
+        heapq.heappush(q, (food_times[i], i + 1)) # 가장 빨리 소모될 수 있는 음식 순서로 삽입
 
-    while f_len > 0:
-        mf_time = min(food_times)
-        if mf_time > k:
-            answer = k % f_len
-            break
-        else:
-            k -= mf_time
-            food_times.pop(food_times.index(min(food_times)))
+    previous = 0
+    while q:
+        temp_time = (q[0][0] - previous) * f_len # 현재 최소의 양을 가지는 음식이 소요되는 시간
+        if temp_time <= k: # k초 이내에 소모 되는 경우
+            k -= temp_time
+            previous = heapq.heappop(q)[0] # 다음 루프에서 사용하기 위해 기록함
             f_len -= 1
-            for i in range(f_len):
-                food_times[i] -= mf_time
+        else: # k초 이내에 소모 불가한 경우 (끝내야 되는 경우)
+            idx = k % f_len
+            q.sort(key=lambda x: x[1]) # 다시 인덱스 순서로 정렬
+            answer = q[idx][1]
+            break
 
-    if f_len == 0:
-        answer = -2
-    return answer + 1
+    return answer
 
-print(solution([3,1,2], 5))
+
+print(solution([3, 2, 2], 5))
