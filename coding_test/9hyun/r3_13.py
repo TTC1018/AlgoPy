eb8196b4277bb9da62c6aa03dbdd9c06c3b29dc9
DongHyeon Park <paramore@kakao.com>
2022-03-16 22:34:23 +0900

    Update r3_13.py

--- coding_test/9hyun/r3_13.py
+++ coding_test/9hyun/r3_13.py
@@ -6,9 +6,9 @@ def calc_dist(combination):
     distance = 0
     for h in house:
         temp_dist = INF
-        for k in combination:
+        for k in combination: # 존재하는 치킨집 중에서 가장 가까운 위치까지의 거리
             temp_dist = min(temp_dist, abs(h[0] - k[0]) + abs(h[1] - k[1]))
-        distance += temp_dist
+        distance += temp_dist # 도시의 치킨거리에 기록
     return distance
 
 
@@ -24,6 +24,6 @@ for i in range(N):
     city.append(row)
 
 answer = INF
-for c in combinations(kfc, M):
+for c in combinations(kfc, M): # 살릴 수 있는 구성
     answer = min(answer, calc_dist(c))
 print(answer)
