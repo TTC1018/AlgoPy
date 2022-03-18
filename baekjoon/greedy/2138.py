7aa06d396f28b461f6ee2f95e644b3aae0b5a8cd
DongHyeon Park <paramore@kakao.com>
2022-03-18 18:21:48 +0900

    백준 그리디 2138 ongoing(2)

--- baekjoon/greedy/2138.py
+++ baekjoon/greedy/2138.py
@@ -1,11 +1,42 @@
+import sys
+from collections import deque
+def reverse(strs):
+    result = ''
+    for s in strs:
+        if s == '0':
+            result += '1'
+        else:
+            result += '0'
+    return result
+
 N = int(input())
-switch, light = [False] * N, [False] * N
+origin, target = input(), input()
 
-s_ipt = list(map(int, input()))
-for i in range(N):
-    if s_ipt[i] == 0:
-        switch[i] = False
-    else:
-        switch[i] = True
+visited = [origin]
+next = deque()
+next.append((origin, 0))
 
+answer = sys.maxsize
+while next:
+    now, count = next.popleft()
 
+    cand = ''
+    for i in range(N):
+        if i == 0:
+            cand = reverse(now[:2]) + now[2:]
+        elif i == N - 1:
+            cand = now[:N - 1] + reverse(now[N - 1:])
+        else:
+            cand = now[:i - 1] + reverse(now[i - 1:i + 2]) + now[i + 2:]
+
+        if not (cand in visited):
+            visited.append(cand)
+            if cand == target:
+                answer = min(answer, count + 1)
+            else:
+                next.append((cand, count + 1))
+
+if answer != sys.maxsize:
+    print(answer)
+else:
+    print(-1)
