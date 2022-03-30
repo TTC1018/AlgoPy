22362085a4c7e511f65faba350d4424c57cc34c8
DongHyeon Park <paramore@kakao.com>
2022-03-30 16:47:32 +0900

    백준 그리디 1461

--- baekjoon/greedy/1461.py
+++ baekjoon/greedy/1461.py
@@ -5,28 +5,30 @@ locs.sort()
 loc_m = [i for i in locs if i < 0]
 loc_p = [i for i in locs if i > 0]
 
-answer = 0
-m_last, p_last = 0, 0
+targets = []
 while loc_m:
     if len(loc_m) > M:
-        answer += (abs(loc_m[0]) * 2)
-        for _ in range(M):
-            loc_m.pop(0)
+        temp_t = []
+        for i in range(M):
+            temp_t.append(loc_m.pop(0))
+        targets.append(temp_t)
     else:
-        start, end = loc_m[0], loc_m[-1]
-        answer += (abs(start) + abs(start - end))
-        m_last = abs(loc_m[-1])
+        targets.append(loc_m)
         break
 while loc_p:
     if len(loc_p) > M:
-        answer += (abs(loc_p[-1]) * 2)
-        for _ in range(M):
-            loc_p.pop()
+        temp_t = []
+        for i in range(M):
+            temp_t.append(loc_p.pop())
+        targets.append(temp_t)
     else:
-        start, end = loc_p[-1], loc_p[0]
-        answer += (start + (start - end))
-        p_last = end
+        loc_p.reverse()
+        targets.append(loc_p)
         break
 
-answer += min(m_last, p_last)
+targets.sort()
+answer = 0
+for t in targets:
+    answer += (abs(t[0]) * 2)
+answer -= max(abs(targets[0][0]), abs(targets[-1][0]))
 print(answer)
