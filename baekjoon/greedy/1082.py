bc9d3cc40d4e455c69ecf071f49e79e3cc30f1f2
DongHyeon Park <paramore@kakao.com>
2022-04-05 18:34:55 +0900

    백준 1082 ongoing (2)

--- baekjoon/greedy/1082.py
+++ baekjoon/greedy/1082.py
@@ -2,3 +2,9 @@ N = int(input())
 P = list(map(int, input().split()))
 M = int(input())
 
+# 가장 큰 수를 고르는 게 이득
+# 그러나 자릿 수가 늘어나는 게 큰 수 하나보다 나음
+dp = [''] * (M + 1)
+for i in range(N):
+    dp[P[i]] += str(i)
+print(dp)
