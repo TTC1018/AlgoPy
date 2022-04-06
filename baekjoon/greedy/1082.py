1fae845df0f33ca59221499c06de1378cc22911c
DongHyeon Park <paramore@kakao.com>
2022-04-06 12:35:59 +0900

    백준 그리디/DP 1082

--- baekjoon/greedy/1082.py
+++ baekjoon/greedy/1082.py
@@ -2,9 +2,9 @@ N = int(input())
 P = list(map(int, input().split()))
 M = int(input())
 
-# 가장 큰 수를 고르는 게 이득
-# 그러나 자릿 수가 늘어나는 게 큰 수 하나보다 나음
-dp = [''] * (M + 1)
-for i in range(N):
-    dp[P[i]] += str(i)
-print(dp)
+dp = [-5001] * (M + 1)
+for i in range(N - 1, -1, -1): # 번호 역순으로
+    target = P[i]
+    for j in range(target, M + 1):
+        dp[j] = max(dp[j], dp[j-target]*10 + i, i)
+print(dp[M])
