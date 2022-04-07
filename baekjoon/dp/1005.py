3ba2d17b7f9b80a6ea2860e2986a296d84d9129e
DongHyeon Park <paramore@kakao.com>
2022-04-07 12:34:41 +0900

    백준 DP 1005

--- baekjoon/dp/1005.py
+++ baekjoon/dp/1005.py
@@ -1,15 +1,10 @@
-def count_parent(num):
-    count = 0
-    count += len(parents[num])
-    for next in parents[num]:
-        count += count_parent(next)
-    return count
-
-
-def dynamic(num):
-    for next in parents[num]:
-            dp[next] = max(dp[next], dp[num] + D[next])
-            dynamic(next)
+def dynamic_dfs(num):
+    if dp[num] == -1:
+        prevMax = 0
+        for child in childs[num]:
+            prevMax = max(prevMax, dynamic_dfs(child))
+        dp[num] = prevMax + D[num]
+    return dp[num]
 
 
 INF = int(1e9)
@@ -17,24 +12,13 @@ for _ in range(int(input())):
     N, K = map(int, input().split())
     D = [-1] + list(map(int, input().split()))
     
-    parents = [[] for _ in range(N + 1)]
+    childs = [[] for _ in range(N + 1)]
     for i in range(K):
         X, Y = map(int, input().split())
-        parents[X].append(Y)
-        
+        childs[Y].append(X)
     W = int(input())
     
-    dp = [0] * (N + 1)
+    dp = [-1] * (N + 1)
     for i in range(1, N + 1):
-        dp[i] = D[i]
-    
-    start = 1
-    count = 0
-    for i in range(1, N + 1):
-        temp_count = count_parent(i)
-        if count < temp_count:
-            count = temp_count
-            start = i
-    
-    dynamic(start)
+        dynamic_dfs(i)
     print(dp[W])
