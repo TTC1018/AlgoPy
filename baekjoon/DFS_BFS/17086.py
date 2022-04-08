b64cfbff95a969038ac392cecee77147d83f5236
DongHyeon Park <paramore@kakao.com>
2022-04-08 23:46:58 +0900

    백준 BFS 17086

--- baekjoon/DFS_BFS/17086.py
+++ baekjoon/DFS_BFS/17086.py
@@ -1,36 +1,31 @@
+from collections import deque
+
 INF = int(1e9)
 direction = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-def dfs(start, count):
-    global answer
+N, M = map(int, input().split())
+q = deque()
+graph = []
+for i in range(N):
+    data = list(map(int, input().split()))
+    for j in range(M):
+        if data[j] == 1:
+            q.append(((i, j), 0))
+    graph.append(data)
 
-    x, y = start
-    if count >= answer[x][y]:
-        return
+
+while q:
+    s, count = q.popleft()
+    x, y = s
 
     for d in direction:
         nx, ny = x + d[0], y + d[1]
         if 0 <= nx < N and 0 <= ny < M:
             if graph[nx][ny] == 0:
-                if not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    answer[nx][ny] = min(answer[nx][ny], count + 1)
-                    dfs((nx, ny), count + 1)
-                    visited[nx][ny] = False
-
-
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-
-answer = [[INF] * M for _ in range(N)]
-visited = [[False] * M for _ in range(N)]
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 1:
-            dfs((i, j), 0)
+                graph[nx][ny] = count + 1
+                q.append(((nx, ny), count + 1))
 
 result = 0
 for i in range(N):
     for j in range(M):
-        if answer[i][j] != INF:
-            result = max(result, answer[i][j])
+        result = max(result, graph[i][j])
 print(result)
--- programmers/양과늑대.py
+++ programmers/양과늑대.py
@@ -3,33 +3,34 @@ childs = []
 answer = 0
 visited = []
 
+
 def dfs(now, sheep, wolf, nexts):
     global info_list, answer, visited
-    
-    if info_list[now] == 0: # 양일 때
+
+    if info_list[now] == 0:  # 양일 때
         sheep += 1
         answer = max(answer, sheep)
     else:
         wolf += 1
-        
-    if sheep <= wolf: # 늑대 수가 양 이상이 됐을 때
+
+    if sheep <= wolf:  # 늑대 수가 양 이상이 됐을 때
         return
-    
+
     nexts.extend(childs[now])
     for next in nexts:
         visited[next] = True
         dfs(next, sheep, wolf, [n for n in nexts if n != next and not visited[n]])
         visited[next] = False
-    
-    
+
+
 def solution(info, edges):
     global info_list, childs, visited
-    info_list = info    
+    info_list = info
     childs = [[] for _ in range(len(info))]
     for e in edges:
         p, c = e
         childs[p].append(c)
-        
+
     visited = [False] * len(info)
     visited[0] = True
     dfs(0, 0, 0, [])
