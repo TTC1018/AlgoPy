d60cf26cea53b7793c6fd71831dda8d30b12e62b
DongHyeon Park <paramore@kakao.com>
2022-04-12 23:21:15 +0900

    백준 구현/DFS 15684
    가로선 3개 이하의 경우만 구하면 되므로 그 구간만 계산

--- baekjoon/9hyun/15684.py
+++ baekjoon/9hyun/15684.py
@@ -1,35 +1,53 @@
+import sys
+input = sys.stdin.readline
 INF = int(1e9)
 # 위치 좌표는 가로선 세로선 교차점이므로
-# M * N 개 존재
-# 가능한 진행 경로 = 아래(-1, 0) OR 오른쪽 (0, 1) OR 왼쪽 (0, -1)
-direc = [(0, 1), (0, -1), (-1, 0)]
+# H * N 개 존재
 def laddering():
-    for i in range(M):
-        x, y = 0, i
-        for d in direc:
-            nx, ny = x + d[0], y + d[1]
-            if 0 <= nx < M and 0 <= ny < N:
-                pass
+    for i in range(N):
+        y = i
+        for x in range(H):
+            if y < N - 1 and widths[x][y]:
+                y += 1
+            elif y > 0 and widths[x][y - 1]:
+                y -= 1
+        if y != i:
+            return False
+    return True
 
 
-def dfs(col, pos, count):
+def dfs(idx, count, limit):
     global answer
 
-    x, y = pos
-    if y == M - 1:
-        if x == col:
-            answer = min(answer, count)
-        return
-    if count == H:
+    if count == limit:
+        if laddering():
+            answer = count
         return
 
-
+    for i in range(idx, H):
+        for j in range(N - 1):
+            if not widths[i][j]:
+                widths[i][j] = True
+                dfs(i, count + 1, limit)
+                widths[i][j] = False
 
 
 N, M, H = map(int, input().split())
-# N - 1 = 열 개수, M = 행 개수
+# N = 열 개수, H = 행 개수
 # 두 가로선이 연속되면 안 됨
 # i번의 결과가 i번에 나오도록 하기
-
-widths = [[False] * (N - 1) for _ in range(M)] # 가로선
+widths = [[False] * (N - 1) for _ in range(H)] # 가로선
 answer = INF
+for _ in range(M):
+    x, y = map(int, input().split())
+    widths[x - 1][y - 1] = True
+
+for i in range(4):
+    dfs(0, 0, i)
+    if answer != INF:
+        break
+
+if answer == INF:
+    print(-1)
+else:
+    print(answer)
