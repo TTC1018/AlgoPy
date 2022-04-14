af7ff2a8392ef919de798314c77f0c7b2c97e936
DongHyeon Park <paramore@kakao.com>
2022-04-14 17:26:13 +0900

    백준 서로소집합 1717
    부모 찾기할 때마다 부모 갱신하는 것으로 시간 초과 해결

---
+++ baekjoon/graph_etc/1717.py
@@ -0,0 +1,37 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def find_parent(node):
+    if parent[node] == node:
+        return node
+    p_node = find_parent(parent[node])
+    parent[node] = p_node
+    return parent[node]
+
+
+def union_parent(a, b):
+    pa = find_parent(a)
+    pb = find_parent(b)
+    
+    if pa == pb:
+        return
+    if pa < pb:
+        parent[pb] = pa
+    else:
+        parent[pa] = pb
+
+
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+
+for _ in range(m):
+    func, a, b = map(int, input().split())
+    
+    if func == 0:
+        union_parent(a, b)
+    else:
+        if find_parent(a) == find_parent(b):
+            print("YES")
+        else:
+            print("NO")
