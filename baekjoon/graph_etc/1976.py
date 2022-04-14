0d76b67f16530f88332892f5eb4d568dd50d33aa
DongHyeon Park <paramore@kakao.com>
2022-04-14 17:44:03 +0900

    백준 서로소집합 1976
    서로소 집합을 이용한 연결 경로 판단

---
+++ baekjoon/graph_etc/1976.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(200)
+
+def find_parent(node):
+    if parent[node] != node:
+        return find_parent(parent[node])
+    return parent[node]
+
+
+def union_parent(a, b):
+    pa = find_parent(a)
+    pb = find_parent(b)
+    
+    if pa == pb:
+        return
+    if pa < pb:
+        parent[pb] = pa
+    else:
+        parent[pa] = pb
+
+N, M = int(input()), int(input())
+parent = [i for i in range(N + 1)]
+for i in range(N):
+    data = list(map(int, input().split()))
+    for j in range(N):
+        if data[j] == 1:
+            union_parent(i + 1, j + 1)
+
+routes = list(map(int, input().split()))
+
+flag = True
+for i in range(1, len(routes)):
+    if find_parent(routes[i]) != find_parent(routes[i - 1]):
+        flag = False
+        break
+
+if flag:
+    print('YES')
+else:
+    print('NO')
