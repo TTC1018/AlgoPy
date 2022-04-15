31a69d52ce636f795dde8331b23f2d18aff250af
DongHyeon Park <paramore@kakao.com>
2022-04-15 09:40:19 +0900

    백준 그래프이론 16562
    서로소 집합 + 방문처리를 이용한 문제풀이

---
+++ baekjoon/graph_etc/16562.py
@@ -0,0 +1,41 @@
+import sys
+from copy import deepcopy
+input = sys.stdin.readline
+
+def find_parent(node):
+    if parent[node] != node:
+        parent[node] = find_parent(parent[node])
+    return parent[node]
+
+
+def union_parent(a, b):
+    pa = find_parent(a)
+    pb = find_parent(b)
+    
+    if pa == pb:
+        return
+    if A[pa] < A[pb]:
+        parent[pb] = pa
+    else:
+        parent[pa] = pb
+
+
+N, M, k = map(int, input().split())
+A = [0] + list(map(int, input().split()))
+parent = [i for i in range(N + 1)]
+min_cost = deepcopy(A)
+for _ in range(M):
+    v, w = map(int, input().split())
+    union_parent(v, w)
+    
+answer = 0
+for i in range(1, N + 1):
+    target = find_parent(i)
+    if target != 0:
+        answer += A[target]
+        union_parent(0, target) # 방문처리
+
+if answer > k:
+    print("Oh no")
+else:
+    print(answer)
