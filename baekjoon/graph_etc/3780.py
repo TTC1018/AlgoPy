051674fbfdaf73291abca6f2c632b71a9d9c4d5c
DongHyeon Park <paramore@kakao.com>
2022-04-19 15:45:59 +0900

    백준 그래프이론 3780

--- baekjoon/graph_etc/3780.py
+++ baekjoon/graph_etc/3780.py
@@ -1,10 +1,24 @@
 import sys
 input = sys.stdin.readline
+def find_parent(node):
+    if parent[node] == node:
+        return node
+    
+    new_parent = find_parent(parent[node]) # 최종 부모
+    dist[node] += dist[parent[node]] # 부모까지 거치는 길이 누적하기
+    parent[node] = new_parent
+    return parent[node]
 
+
+def union_parent(a, b):
+    dist[a] = abs(a - b) % 1000
+    parent[a] = b
 
 
 for _ in range(int(input())):
     N = int(input())
+    parent = [i for i in range(N + 1)]
+    dist = [0 for _ in range(N + 1)] # 특정 기업에서 센터까지의 길이
     while True:
         data = input().split()
         op = data[0]
@@ -12,6 +26,13 @@ for _ in range(int(input())):
         if op == 'O':
             break
         elif op == 'E':
-            pass
+            I = int(data[1])
+            find_parent(I) # 최종 부모로 갱신
+            print(dist)
+            print(dist[I])
+            
         elif op == 'I':
-            pass
+            I, J = map(int, data[1:]) 
+            # I는 부모, J는 특정 부모의 자식
+            # J의 부모에 union
+            union_parent(I, J)
