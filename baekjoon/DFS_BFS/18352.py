7d0a942e46097ce877d74304521e91e11ce32567
DongHyeon Park <paramore@kakao.com>
2022-04-28 17:56:54 +0900

    백준 BFS 18352 ongoing

---
+++ baekjoon/DFS_BFS/18352.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+N, M, K, X = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+for _ in range(M):
+    A, B = map(int, input().split())
+    graph[A].append(B)
+answer = []
+visited = [False] * (N + 1)
+
+q = deque()
+q.append((X, 0))
+visited[X] = True
+
+while q:
+    now, dist = q.popleft()
+    if dist == K:
+        answer.append(now)
+        continue
+    
+    for nxt in graph[now]:
+        if not visited[nxt]:
+            visited[nxt] = True
+            q.append((nxt, dist + 1))
+
+if len(answer) > 0:
+    for a in answer:
+        print(a)
+else:
+    print(-1)
