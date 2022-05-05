0d0f2b8ed251995eb7c1e50867cda659d503e77c
DongHyeon Park <paramore@kakao.com>
2022-05-06 00:53:17 +0900

    카카오 인턴십 2021 미로탈출

--- programmers/kakao/미로탈출.py
+++ programmers/kakao/미로탈출.py
@@ -1,43 +1,56 @@
 from heapq import heappush, heappop
-from copy import deepcopy
 
 
 def solution(n, start, end, roads, traps):
     # 방의 개수 / 출발 방 / 도착 방 / 통로와 이동시간 배열 / 함정 방 번호
     INF = int(1e9)
 
-    graph = [[] for _ in range(n + 1)]
+    graph = [[INF] * (n + 1) for _ in range(n + 1)]
     for r in roads:
         P, Q, S = r
-        graph[P].append((Q, S, True))
-        graph[Q].append((P, S, False))
+        if S < graph[P][Q]:
+            graph[P][Q] = S
+    for i in range(1, n + 1):
+        graph[i][i] = 0
 
-    q = [(0, start, deepcopy(graph))]
-    distance = [INF] * (n + 1)
-    distance[start] = 0
+    visited = [[False] * (1 << len(traps)) for _ in range(n + 1)]
+    q = [(0, start, 0)]
     while q:
-        dist, now, now_graph = heappop(q)
-        if distance[now] < dist:
-            continue
+        dist, now, visit = heappop(q)
         if now == end:
+            return dist
+
+        if visited[now][visit]:
             continue
-        if now in traps:
-            for nxt, cst, can in now_graph[now]:
-                now_graph[nxt].remove((now, cst, not can))
-                now_graph[nxt].append((now, cst, can))
-            n_len = len(now_graph[now])
-            for i in range(n_len):
-                nxt, cst, can = now_graph[now][i]
-                now_graph[now][i] = (nxt, cst, not can)
-        for nxt, cst, can in now_graph[now]:
-            if can:
-                n_dist = dist + cst
-                if n_dist < distance[nxt]:
-                    distance[nxt] = n_dist
-                    heappush(q, (n_dist, nxt, deepcopy(now_graph)))
+        visited[now][visit] = True
 
+        now_is_trapped = False
+        trapped_rooms = set()  # 현재 상태에서 발동된 방 저장
+        # 발동된 함정 확인
+        for i in range(len(traps)):
+            bit = 1 << i  # 비트마스킹
+            if visit & bit:  # 발동된 적 있으면
+                if traps[i] == now:
+                    visit &= ~bit  # 지금 방이랑 같으면 다시 꺼줘야 됨
+                else:
+                    trapped_rooms.add(traps[i])
+            else:
+                if traps[i] == now:
+                    visit |= bit  # 지금 방이랑 같으면 켜주기
+                    trapped_rooms.add(traps[i])
+                    now_is_trapped = True
 
-    return distance[end]
+        # 다음 이동장소 확인
+        for nxt in [nxt for nxt in range(1, n + 1) if nxt != now]:
+            nxt_is_trapped = True if nxt in trapped_rooms else False
+            if now_is_trapped == nxt_is_trapped:  # 둘다 켜져있거나 꺼져있으면 정방향
+                if graph[now][nxt] != INF:
+                    heappush(q, (dist + graph[now][nxt], nxt, visit))
+            else:
+                if graph[nxt][now] != INF:  # 역방향
+                    heappush(q, (dist + graph[nxt][now], nxt, visit))
+
+    return INF
 
 
 print(solution(3, 1, 3, [[1, 2, 2], [3, 2, 3]], [2]))
