2cf5b2a93386287c72e125d1cc65c5f02f64bbef
DongHyeon Park <paramore@kakao.com>
2022-05-09 19:04:31 +0900

    백준 구현 15683

--- baekjoon/9hyun/15683.py
+++ baekjoon/9hyun/15683.py
@@ -1,17 +1,18 @@
 from copy import deepcopy
+import sys
+input = sys.stdin.readline
 in_range = lambda x, y: 0 <= x < N and 0 <= y < M
 
 
-def fill(grp_arg, cand, x, y):
+def visit(grp_arg, cand, x, y):
     for d in cand:
-        nx, ny = x, y
-        while True:
+        nx, ny = x + direc[d][0], y + direc[d][1]
+        while in_range(nx, ny):
+            if grp_arg[nx][ny] == 6:
+                break
+            elif not grp_arg[nx][ny]:
+                grp_arg[nx][ny] = -1
             nx, ny = nx + direc[d][0], ny + direc[d][1]
-            if in_range(nx, ny):
-                if grp_arg[nx][ny] == 6:
-                    break
-                elif not grp_arg[nx][ny]:
-                    grp_arg[nx][ny] = -1
 
 
 def dfs(grp_arg, count):
@@ -19,22 +20,20 @@ def dfs(grp_arg, count):
 
     if count == limit:
         result = 0
-        for cand in range(N):
+        for i in range(N):
             for j in range(M):
-                if grp_arg[cand][j] == 0:
+                if grp_arg[i][j] == 0:
                     result += 1
         answer = min(answer, result)
         return
 
-
     num, x, y = cctv[count]
-    grp_cpy = deepcopy(grp_arg)
     for cand in cctv_direc[num]:
-        fill(grp_cpy, cand, x, y)
-        dfs(grp_cpy, count + 1)
         grp_cpy = deepcopy(grp_arg)
+        visit(grp_cpy, cand, x, y)
+        dfs(grp_cpy, count + 1)
+        
 
-  
 direc = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 limit = 0    
 N, M = map(int, input().split())
@@ -56,8 +55,6 @@ cctv_direc = [
     [[0, 1, 2], [0, 1, 3], [1, 2, 3], [0, 2, 3]],
     [[i for i in range(4)]],
 ]
-
-
 
 answer = int(1e9)
 dfs(graph, 0)
