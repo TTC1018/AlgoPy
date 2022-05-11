684c29cd2a6fb6a8eedcb91cc48df012b1a31a50
DongHyeon Park <paramore@kakao.com>
2022-05-11 21:20:25 +0900

    Update 17825.py

--- baekjoon/9hyun/17825.py
+++ baekjoon/9hyun/17825.py
@@ -1,89 +1,60 @@
-import sys
-input = sys.stdin.readline
+def init_field():
+    global way, scores
+    scores = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, # 0 ~ 10
+              22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 0, # 11 ~ 21
+              13, 16, 19, 25, 22, 24, 28, 27, 26, 30, 35] # 22 ~ 32
+    way = {}
+    for i in range(21):
+        lst = []
+        for j in range(5):
+            if i + j + 1 >= 21:
+                lst.append(21)
+                break
+            else:
+                lst.append(i + j + 1)
+        way[i] = lst
+    line_10 = [5, 22, 23, 24, 25, 31, 32, 20, 21] # 10 라인
+    line_20 = [10, 26, 27, 25, 31, 32, 20, 21] # 20 라인
+    line_30 = [15, 28, 29, 30, 25, 31, 32, 20, 21] # 30 라인
+
+    for i in range(len(line_10)):
+        way[line_10[i]] = line_10[i + 1:i + 6]
+    for i in range(len(line_20)):
+        way[line_20[i]] = line_20[i + 1:i + 6]
+    for i in range(len(line_30)):
+        way[line_30[i]] = line_30[i + 1:i + 6]
 
 
-def dfs(count, sum_val):
-    global answer
+def dfs(count, score):
 
     if count == 10:
-        answer = max(answer, sum_val)
-        return
+        return score
 
-    dice = dices[count]  # 현재 주사위로 갈 칸
+    dice = dices[count]
+    max_score = score
     for i in range(4):
-        line, pos = piece[i] # 현재 라인, 라인 내 위치
-        end_point = len(board[line]) - 1
-
-        if pos == end_point: # 이미 도착한 말
+        if pieces[i] == 21: # 이미 도착
             continue
-        elif pos + dice >= end_point: # 더 가면 도착 지점인 경우
-            piece[i][1] = end_point
-            dfs(count + 1, sum_val)
-            piece[i][1] = pos
-        else: # 도착 지점 아닌 경우
-            pos = pos + dice
-            score = board[line][pos]  # 도착할 칸 점수
-
-            if line == 0:
-                if pos in [5, 10, 15]:
-                    if [score, 0] in piece:
-                        continue
-                    else:
-                        piece[i] = [score, 0]
-                        dfs(count + 1, sum_val + score)
-                        piece[i] = [line, pos - dice]
-                elif pos == 20:
-                    if [10, 7] in piece or [20, 6] in piece or [30, 7]:
-                        continue
-                    else:
-                        piece[i][1] = pos  # 라인 내 위치 갱신
-                        dfs(count + 1, sum_val + score)
-                        piece[i][1] = pos - dice
-                else:
-                    if [line, pos] in piece:
-                        continue  # 이미 다른 말이 대기중일 때
-
-                    piece[i][1] = pos  # 라인 내 위치 갱신
-                    dfs(count + 1, sum_val + score)
-                    piece[i][1] = pos - dice
-            else:
-                if line == 10:
-                    if [20, pos - 1] in piece or [30, pos] in piece:
-                        continue
-                    elif pos == end_point - 1:
-                        if [0, 20] in piece:
-                            continue
-                elif line == 20:
-                    if [10, pos + 1] in piece or [30, pos + 1] in piece:
-                        continue
-                    elif pos == end_point - 1:
-                        if [0, 20] in piece:
-                            continue
-                elif line == 30:
-                    if [10, pos] in piece or [20, pos - 1] in piece:
-                        continue
-                    elif pos == end_point - 1:
-                        if [0, 20] in piece:
-                            continue
-
-                if [line, pos] in piece:
-                    continue  # 이미 다른 말이 대기중일 때
-
-                piece[i][1] = pos # 라인 내 위치 갱신
-                dfs(count + 1, sum_val + score)
-                piece[i][1] = pos - dice
-
+        if len(way[pieces[i]]) < dice:  # 다음 칸이 도착칸
+            next_index = 21
+        else:
+            next_index = way[pieces[i]][dice - 1]
+        if next_index in pieces and next_index != 21:
+            continue
+        cur_index = pieces[i]
+        pieces[i] = next_index
+        max_score = max(max_score, dfs(count + 1, score + scores[next_index]))
+        pieces[i] = cur_index
+    return max_score
 
 
 dices = list(map(int, input().split()))
-board = {0: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, -1],
-         10: [10, 13, 16, 19, 25, 30, 35, 40, -1],
-         20: [20, 22, 24, 25, 30, 35, 40, -1],
-         30: [30, 28, 27, 26, 25, 30, 35, 40, -1]}
 
+# 출발,도착 포함 33개 / 자식 index, 자신 점수
+way = {}
+pieces = [0, 0, 0, 0]
+scores = []
 
-piece = [[0, 0], [0, 0], [0, 0], [0, 0]] # 어떤 라인의 몇번째 칸에 있는지
-
-answer = 0
-dfs(0, 0)
+init_field()
+answer = dfs(0, 0)
 print(answer)
