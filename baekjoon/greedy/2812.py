5524aaa8b5cc01467458d488d76783f685edd82d
DongHyeon Park <paramore@kakao.com>
2022-05-31 15:14:07 +0900

    Update 2812.py

--- baekjoon/greedy/2812.py
+++ baekjoon/greedy/2812.py
@@ -6,7 +6,7 @@ num = list(map(int, list(sys.stdin.readline().rstrip())))
 
 q = []
 for i in range(N):
-    while K > 0 and q and q[-1] < num[i]:
+    while K > 0 and q and q[-1] < num[i]: # 현재 들어갈 수보다 작은 수들을 스택에서 제거
         q.pop()
         K -= 1
     q.append(num[i])
