ec8eeeb7314352b71e55f975b3106868e97df532
DongHyeon Park <paramore@kakao.com>
2022-05-31 15:13:30 +0900

    백준 그리디 2812

---
+++ baekjoon/greedy/2812.py
@@ -0,0 +1,17 @@
+import sys
+
+
+N, K = map(int, sys.stdin.readline().rstrip().split())
+num = list(map(int, list(sys.stdin.readline().rstrip())))
+
+q = []
+for i in range(N):
+    while K > 0 and q and q[-1] < num[i]:
+        q.pop()
+        K -= 1
+    q.append(num[i])
+
+while K > 0: # 내림차순 삽입 혹은 같은 값의 수 반복 등으로 덜 제거 됐을 때
+    K -= 1
+    q.pop() # 맨 뒤에 수 제거
+print(''.join(map(str, q)))
