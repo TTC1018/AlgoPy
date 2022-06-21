23b03905295fb39b084648b1bda519775183821b
DongHyeon Park <paramore@kakao.com>
2022-06-21 20:49:10 +0900

    백준 문자열 12919

--- baekjoon/string/12919.py
+++ baekjoon/string/12919.py
@@ -1,34 +1,24 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-# BABA
-# BAB + A
-# ABA + B & 뒤집기
-S = list(input().rstrip())
-T = list(input().rstrip())
 
+def btk(s, l):
+    if l == s_len:
+        if s == S:
+            print(1)
+            sys.exit()
+    if l - 1 == 0:
+        return
+
+    if s[-1] == 'A':
+        btk(s[:-1], l - 1)
+
+    if s[0] == 'B':
+        btk(s[1:][::-1], l - 1)
+
+
+S = input().rstrip()
+T = input().rstrip()
 s_len, t_len = len(S), len(T)
-r_flag = False
-
-cnt = t_len
-for i in range(t_len - 1, -1, -1):
-    if T[i] == 'A':
-        T.pop()
-    elif T[i] == 'B':
-        if r_flag:
-            T.pop()
-        else:
-            T.pop(0)
-        r_flag = not r_flag
-
-    cnt -= 1
-    if cnt == s_len:
-        if r_flag:
-            T = T[::-1]
-        break
-
-if S == T:
-    print(1)
-else:
-    print(0)
+btk(T, t_len)
+print(0)
