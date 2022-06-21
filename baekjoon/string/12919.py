db7a33ba564929460c0ad6095d8de4fbbb8bb438
DongHyeon Park <paramore@kakao.com>
2022-06-21 18:21:11 +0900

    백준 문자열 12919 ongoing

---
+++ baekjoon/string/12919.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+S = list(input().rstrip())
+T = list(input().rstrip())
+
+s_len, t_len = len(S), len(T)
+r_flag = False
+
+cnt = t_len
+for i in range(t_len - 1, -1, -1):
+    if T[i] == 'A':
+        T.pop()
+    elif T[i] == 'B':
+        if r_flag:
+            T.pop()
+        else:
+            T.pop(0)
+        r_flag = not r_flag
+
+    cnt -= 1
+    if cnt == s_len:
+        if r_flag:
+            T = T[::-1]
+        break
+
+if S == T:
+    print(1)
+else:
+    print(0)
