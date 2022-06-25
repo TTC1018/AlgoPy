9c40f6f9d8794909997be1a172354d5f5f03217b
DongHyeon Park <paramore@kakao.com>
2022-06-25 15:30:40 +0900

    백준 구현 11559

---
+++ baekjoon/9hyun/11559.py
@@ -0,0 +1,63 @@
+from collections import deque
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+in_range = lambda x, y: 0 <= x < 12 and 0 <= y < 6
+
+
+def explode():
+    global chain
+
+    # for f in field:
+    #     print(*f)
+    # print()
+
+    d_flag = False
+    q = deque()
+    for i in range(11, -1, -1):
+        for j in range(6):
+            if field[i][j] != '.':
+                q.append((i, j))
+                color = field[i][j]
+                visited = [[False] * 6 for _ in range(12)]
+                visited[i][j] = True
+                target = [(i, j)]
+                while q:
+                    x, y = q.popleft()
+
+                    for d in direc:
+                        nx, ny = x + d[0], y + d[1]
+                        if in_range(nx, ny) and field[nx][ny] == color:
+                            if not visited[nx][ny]:
+                                visited[nx][ny] = True
+                                q.append((nx, ny))
+                                target.append((nx, ny))
+
+                if len(target) >= 4:
+                    for x, y in target:
+                        field[x][y] = '.'
+                    d_flag = True
+
+    if d_flag:
+        chain += 1
+        drop()
+
+
+def drop():
+    for j in range(6):
+        for i in range(10, -1, -1):
+            if field[i][j] != '.':
+                x, y = i, j
+                while x < 11 and field[x + 1][y] == '.':
+                    field[x][y], field[x + 1][y] = '.', field[x][y]
+                    x += 1
+    explode()
+
+
+
+direc = [(-1, 0), (0, 1), (0, -1), (1, 0)]
+field = [list(input().rstrip()) for _ in range(12)]
+
+chain = 0
+explode()
+print(chain)
