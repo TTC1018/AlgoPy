5bab99bf5e1450181482ac6d27a48ead9b0eae84
ttc1018 <paramore@kakao.com>
2022-09-04 00:37:51 +0900

    Update 7. 역순연결리스트2.py

--- python_interview/LinkedList/7. 역순연결리스트2.py
+++ python_interview/LinkedList/7. 역순연결리스트2.py
@@ -16,6 +16,10 @@ class Solution:
 
             for _ in range(right - left):
                 tmp, start.next, end.next = start.next, end.next, end.next.next
+                # start의 다음 노드를 임시 저장,
+                # 그 후 start의 다음 노드를 end의 다음 노드로 설정
+                # 그리고 end였던 노드를 한칸 밀어냄 (start.next의 다음 노드로 설정)
+
                 start.next.next = tmp # start의 바로 뒤에 있던 노드가 새로운 노드에게 밀려남 (새 노드의 다음 노드가 됨)
 
         return root.next
