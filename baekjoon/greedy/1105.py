2fef6574aa935cdd151b2bcc7cf18c4cf1046795
DongHyeon Park <paramore@kakao.com>
2023-02-17 14:06:08 +0900

    EDIT: 백준 그리디 1105

--- baekjoon/greedy/1105.py
+++ baekjoon/greedy/1105.py
@@ -1,22 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-L, R = map(int, input().split())
-max_val = len(str(L))
-
-l_len = len(str(L))
-r_len = len(str(R))
+L, R = input().rstrip().split()
+l_len = len(L)
+r_len = len(R)
 
 if l_len < r_len:
     print(0)
 elif l_len == r_len:
     if L == R:
-        print(str(L).count('8'))
+        print(L.count('8'))
     else:
         tmp = 0
         for i in range(l_len):
-            if str(L)[i] != str(R)[i]:
+            if L[i] != R[i]:
                 break
-            if str(L)[i] == '8':
+            if L[i] == '8':
                 tmp += 1
         print(tmp)
